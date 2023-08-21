f9944cc53acb11e47bb2349a7b3f7b568d636e1a
KAI MAURIN-JONES <kmaurinj@student.ubc.ca>
2023-08-21 09:22:41 -0400

    plot updated

--- plot.py
+++ plot.py
@@ -35,33 +35,84 @@ grouped_df_recent['date_str'] = grouped_df_recent['date'].apply(lambda x: get_da
 full_df_recent = pd.concat(full_dfs, axis = 0, ignore_index = True).sort_values(by = 'date', ascending = True).reset_index(drop = True)
 full_df_recent['date_str'] = full_df_recent['date'].apply(lambda x: get_date_str(x))
 
-### THIS IS THE PLOT -- this should be called in the app.py file
+# ### THIS IS THE PLOT -- this should be called in the app.py file
+
+# def show_grand_plot():
+
+#     fig = px.line(grouped_df_recent, x = 'date_str', y = 'compound', color = 'source',
+#             #   color_discrete_map = {"CBC": "#EC1D2D", "CTV": "#0046D4", "Global": "#231F20"},
+#             #   title = "Sentiment Valency over Time",
+#               markers = True)
+
+#     # Updating layout with font sizes and title position
+#     fig.update_layout(
+#         title = {
+#             'text': "Sentiment of Canadian News Outlets Over Time",
+#             'y': 0.95,
+#             'x': 0.5,
+#             'xanchor': 'center',
+#             'yanchor': 'top',
+#             'font': {
+#                 'size': 20  # or any desired font size
+#             }
+#         },
+#         xaxis_title = "Date",
+#         xaxis_title_font_size = 16,  # or any desired font size
+#         xaxis_tickfont_size = 14,  # or any desired font size
+#         yaxis_title = "Sentiment Valence",
+#         yaxis_title_font_size = 16,  # or any desired font size
+#         yaxis_tickfont_size = 14   # or any desired font size
+#     )
+#     return fig
+
+
+
+
+import pandas as pd
+import plotly.express as px
 
 def show_grand_plot():
+    # Assuming that 'date_str' is a string; converting it to datetime for sorting
+    grouped_df_recent['date_str'] = pd.to_datetime(grouped_df_recent['date_str'])
+    grouped_df_recent.sort_values('date_str', inplace=True)
 
-    fig = px.line(grouped_df_recent, x = 'date_str', y = 'compound', color = 'source',
-            #   color_discrete_map = {"CBC": "#EC1D2D", "CTV": "#0046D4", "Global": "#231F20"},
-            #   title = "Sentiment Valency over Time",
-              markers = True)
+    # Identify the last 7 days
+    last_day = grouped_df_recent['date_str'].max()
+    first_day_of_last_week = last_day - pd.Timedelta(days=6)
 
-    # Updating layout with font sizes and title position
+    fig = px.line(grouped_df_recent, x='date_str', y='compound', color='source', markers=True)
+
+    # Updating layout
     fig.update_layout(
-        title = {
+        title={
             'text': "Sentiment of Canadian News Outlets Over Time",
             'y': 0.95,
             'x': 0.5,
             'xanchor': 'center',
             'yanchor': 'top',
-            'font': {
-                'size': 20  # or any desired font size
-            }
+            'font': {'size': 20}
         },
-        xaxis_title = "Date",
-        xaxis_title_font_size = 16,  # or any desired font size
-        xaxis_tickfont_size = 14,  # or any desired font size
-        yaxis_title = "Sentiment Valence",
-        yaxis_title_font_size = 16,  # or any desired font size
-        yaxis_tickfont_size = 14   # or any desired font size
+        xaxis_title="Date",
+        xaxis_title_font_size=16,
+        xaxis_tickfont_size=14,
+        yaxis_title="Sentiment Valence",
+        yaxis_title_font_size=16,
+        yaxis_tickfont_size=14,
+        # Adding date range selector
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=7, label="1w", step="day", stepmode="backward"),
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=3, label="3m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(step="all", label="All")
+                ])
+            ),
+            rangeslider=dict(visible=True),
+            type="date",
+            # Default to the last 7 days
+            range=[first_day_of_last_week, last_day]
+        )
     )
-
     return fig
